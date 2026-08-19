9fd51115ade96b28818301e3a48351fae53659ea
Juan_Camacho <jcamachomolina503@gmail.com>
2026-08-19 11:38:17 -0500

    Refactor death counter initialization and enhance game data saving/loading functionality

--- Scenes/Interface/deaths_counter.gd
+++ Scenes/Interface/deaths_counter.gd
@@ -4,6 +4,7 @@ extends Control
 
 func _ready() -> void:
 	GlobalController.death_count_changed.connect(_current_text)
+	_current_text()
 
 func _current_text():
 	label.text = str(GlobalController.death_count)
--- Scenes/controller_game/controller_game.gd
+++ Scenes/controller_game/controller_game.gd
@@ -3,5 +3,20 @@ extends Node
 
 @export var game_data: GameData
 
-var _route: String = "user://game_data.tres"
+var _route: String = "user://game.tres"
 
+func _save_game_data() -> void:
+    game_data.level = GlobalController.level
+    game_data.death_count = GlobalController.death_count
+
+    ResourceSaver.save(game_data, _route)
+
+func _load_game_data() -> void:
+    if ResourceLoader.exists(_route):
+        game_data = load(_route)
+
+        GlobalController.level = game_data.level
+        GlobalController.death_count = game_data.death_count
+    else:
+        GlobalController.level = 1
+        GlobalController.death_count = 0
